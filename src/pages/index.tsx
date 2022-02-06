a002af544fe42a82280ef6324d718c44e6768dda
ptrkvsky <johan.petrikovsky@gmail.com>
2022-02-06 23:58:46 +0100

    fix: update home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,15 @@
-import { Typography, Container } from '@mui/material';
+import { LayoutMain } from '@/components';
 import Head from 'next/head';
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Nomad</title>
+        <title>Accueil</title>
         <meta name="description" content="Nomad" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
-        <Container maxWidth="xl">
-          <Typography variant="h3" sx={{ mt: 4 }} component="div" gutterBottom>
-            Nomad
-          </Typography>
-          <p>
-            Consulter le menu pour ajouter un message ou rechercher un appareil
-            <br />
-          </p>
-        </Container>
-      </div>
+      <LayoutMain title="Accueil">TODO</LayoutMain>
     </div>
   );
 }
